374ee9b4d60a0f15da9131753657320f0d37e770
964505586 <964505586@qq.com>
2019-12-07 20:03:47 +0800

    xbjy_

--- src/com/dfbz/sys/utils/DBUtil.java
+++ src/com/dfbz/sys/utils/DBUtil.java
@@ -23,11 +23,6 @@ public class DBUtil {
 
     private static DataSource dataSource;
 
-    public static void main(String[] args) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
-        System.out.println("=======测试jdbcTemplate:" + jdbcTemplate);
-    }
-
     static {
         Properties pro = new Properties();
         InputStream in = DBUtil.class.getResourceAsStream("/com/db.properties");
@@ -49,44 +44,4 @@ public class DBUtil {
     public static DataSource getDataSource() {
         return dataSource;
     }
-
-    /**
-     * 获取单个连接
-     */
-    public static Connection getConnection() {
-        try {
-            return dataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * 关闭资源
-     */
-    public static void close(Connection conn, Statement stat, ResultSet rs) {
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if (stat != null) {
-            try {
-                stat.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
